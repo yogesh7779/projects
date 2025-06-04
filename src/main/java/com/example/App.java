254a0587dc0ee2616b25c227e37268e3fea84985
M Yogeshwaran <yogeshwaran9606@gmail.com>
2025-06-04 20:59:37 +0530

    Added feature message in feature-addition branch

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("This is a new feature from the feature-addition branch.");
+ );
     }
 }
