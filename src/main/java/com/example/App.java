b74a0d90a347a16334797859ad02f3642f94fb6a
M Yogeshwaran <yogeshwaran9606@gmail.com>
2025-06-04 22:46:02 +0530

    Fixed syntax error in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,14 +1,11 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("This is a new feature from the feature-addition branch.");
- );
+public class App {
+    public static void main(String[] args) {
+        double num1 = 5;
+        double num2 = 10;
+        double sum = num1 + num2;
+
+        System.out.printf("The sum of %.2f and %.2f is %.2f%n", num1, num2, sum);
     }
 }
